02f6062212a2f46a3ac6147c3bc8e27cb073f82b
roshangrewal <roshangrewal@hotmail.com>
2020-04-21 20:55:48 +0530

    final working extension bookmark and history

--- covid-tracker/popup.js
+++ covid-tracker/popup.js
@@ -5,6 +5,10 @@ fetch('https://corona.lmao.ninja/v2/countries/India')
 .then(data =>{
     console.log(data); 
     document.getElementById('totalDeaths').textContent += data.deaths;
-    document.getElementById('totalCases').textContent += data.cases
-    document.getElementById('activeCases').textContent += data.active
+    document.getElementById('totalCases').textContent += data.cases;
+    document.getElementById('activeCases').textContent += data.active;
+
+    document.getElementById('todayCases').textContent += data.todayCases;
+    document.getElementById('tests').textContent += data.tests;
+
 })
